90842c484c696af40343428bbeb9fb842bb8efd1
chani <click0548450396@gmail.com>
2018-02-20 01:20:17 +0200

    1

--- app/client/shop/shop.controller.js
+++ app/client/shop/shop.controller.js
@@ -2,7 +2,7 @@ App.controller('shop', function($scope, $rootScope, $location, commonData, appSe
     //Checks if a user is logged in
     let checkIflogedin = commonData.getData();
     if (!checkIflogedin.logedin) $location.path("/");
-
+    if (checkIflogedin.shopping_cart.length <= 0) $scope.empty = true;
 
     //listens to a broascast logout event
     $scope.$on('logout', function(event, args) {
@@ -11,6 +11,54 @@ App.controller('shop', function($scope, $rootScope, $location, commonData, appSe
 
     });
     $scope.content = 'Hello World';
+    $scope.products = [{
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        }, {
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        }, {
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        }, {
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        }, {
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        }, {
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        }, {
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        }, {
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        }, {
+            name: "apple",
+            image: "apple.jpg",
+            description: "red Hermon apple",
+            price: 2.5
+        },
+
+    ]
 
 
 
